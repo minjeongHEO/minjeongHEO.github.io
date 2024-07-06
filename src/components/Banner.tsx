f2554c94c83d349a3d7ed6cc31ce625ca43b36b4
minjeongHEO <hmjoung9770@gmail.com>
2024-07-07 02:19:47 +0900

    feat: 배너 생성

---
+++ src/components/Banner.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Banner() {
+  return (
+    <div className="min-w-96 w-full h-24 align-content-center">
+      <div>
+        <section>
+          <span className="font-black" style={{ fontSize: 'xxx-large' }}>
+            Woody.Tech(
+            <span className="mx-4">
+              <span data-text="FrontEnd">FrontEnd</span>
+            </span>
+            )
+          </span>
+        </section>
+      </div>
+    </div>
+  );
+}
